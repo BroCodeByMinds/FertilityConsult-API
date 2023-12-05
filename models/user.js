4ceefa89b2303563a35aaadc79c119d5b7e64d25
BroCodeByMinds <brocodebyminds@gmail.com>
2023-12-06 01:06:36 +0530

    check User Registration

--- models/user.js
+++ models/user.js
@@ -1,11 +1,14 @@
 // models/user.js
 const db = require('../db');
+const logger = require('../logger');
 
 class User {
   static createUser(userData) {
     return new Promise((resolve, reject) => {
       const sql = 'INSERT INTO fertility_consult.customer (name, email, password) VALUES (?, ?, ?)';
       const values = [userData.name, userData.email, userData.password];
+      
+      logger.info(`create user SQL : ${sql} and values : ${values}`)
 
       db.query(sql, values, (err, result) => {
         if (err) {
@@ -16,6 +19,37 @@ class User {
       });
     });
   }
+
+  static checkUserRegistration(userData) {
+    return new Promise((resolve, reject) => {
+      const sql = 'SELECT * FROM fertility_consult.customer WHERE email = ? AND password = ?';
+      const values = [userData.email, userData.password];
+  
+      logger.info(`check user SQL : ${sql} and values : ${JSON.stringify(values)}`);
+  
+      db.query(sql, values, (err, result) => {
+        if (err) {
+          logger.error(`checkUserRegistration error: ${err}`);
+          reject(err);
+          return;
+        }
+  
+        if (result.length > 0) {
+          logger.info(`User found in DB. Row count: ${result.length}`);
+          result.forEach((row, index) => {
+            logger.info(`Row ${index + 1}: ${JSON.stringify(row)}`);
+          });
+  
+          // If user data is found, send the user data instead of just true
+          resolve(result); // Sending the user data array
+        } else {
+          logger.info('User not found in DB.');
+          resolve(result);
+        }
+      });
+    });
+  }
+  
 }
 
-module.exports = User;
+module.exports = User
